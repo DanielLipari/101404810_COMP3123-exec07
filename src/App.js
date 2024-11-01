fe592c3f5123ffc1389484182c4b93741476257a
Taemer <daniel.lipari@georgebrown.ca>
2024-11-01 15:42:13 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [title, setTitle] = useState("Welcome to Fullstack Development - I");
+  const [description, setDescription] = useState("React JS Programming Week09 Lab Exercise");
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{title}</h1>
+        <p>{description}</p>
+        <MyInfo name="Daniel Lipari" studentId="101404810" />
       </header>
+    </div>
+  );
+}
+
+function MyInfo(props) {
+  return (
+    <div>
+      <h2>My Information</h2>
+      <p>Name: {props.name}</p>
+      <p>Student ID: {props.studentId}</p>
+      <p>George Brown College, Toronto</p>
     </div>
   );
 }
